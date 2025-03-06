9a417727e9b7825930890f19495cca5712be5ee4
Sahil Dhage <sahildhage555@gmail.com>
2025-03-06 17:42:42 +0530

    small changes

--- Backend/index.js
+++ Backend/index.js
@@ -24,5 +24,5 @@ app.use("/api/products", productRoutes);
 app.use("/api/footer", footerRoutes); // ✅ Footer Content API
 
 // ✅ Start the Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
